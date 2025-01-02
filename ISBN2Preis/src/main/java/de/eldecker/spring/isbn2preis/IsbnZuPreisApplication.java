68e0814fcd6f211e7bb3adce879e2354b39a5b48
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-01-02 17:36:04 +0100

    ISBN2Preis: Projekt von Spring Initializr mit etwas Nacharbeit https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.4.1&packaging=jar&jvmVersion=23&groupId=de.eldecker.spring&artifactId=isbn2preis&name=ISBN-zu-Preis&description=REST-API%20zur%20Preisabfrage%20von%20B%C3%BCchern%20anhand%20ISBN-13&packageName=de.eldecker.spring.isbn2preis&dependencies=web

---
+++ ISBN2Preis/src/main/java/de/eldecker/spring/isbn2preis/IsbnZuPreisApplication.java
@@ -0,0 +1,13 @@
+package de.eldecker.spring.isbn2preis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsbnZuPreisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsbnZuPreisApplication.class, args);
+	}
+
+}
