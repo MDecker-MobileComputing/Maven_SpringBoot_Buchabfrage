f1eaa1e01b0f91f370c462fd9eac07a488034c42
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-01-03 13:54:07 +0100

    Buchabfrage: Projekt neu aufgesetzt
    https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.4.1&packaging=jar&jvmVersion=23&groupId=de.eldecker.spring&artifactId=buchabfrage&name=Buchabfrage&description=Buchabfrage%20anhand%20ISBN13&packageName=de.eldecker.spring.buchabfrage&dependencies=web,thymeleaf,cloud-loadbalancer

--- Buchabfrage/src/main/java/de/eldecker/spring/buchabfrage/BuchabfrageApplication.java
+++ Buchabfrage/src/main/java/de/eldecker/spring/buchabfrage/BuchabfrageApplication.java
@@ -3,21 +3,11 @@ package de.eldecker.spring.buchabfrage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-/**
- * Einstiegspunkt für Programm.
- */
 @SpringBootApplication
 public class BuchabfrageApplication {
 
-    /**
-     * Einstiegsmethode für Programmausführung.
-     *
-     * @param args Kommandozeilenargumente, werden an Spring durchgereicht
-     */
-    public static void main( String[] args ) {
-
-        SpringApplication.run( BuchabfrageApplication.class, args );
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BuchabfrageApplication.class, args);
+	}
 
 }
