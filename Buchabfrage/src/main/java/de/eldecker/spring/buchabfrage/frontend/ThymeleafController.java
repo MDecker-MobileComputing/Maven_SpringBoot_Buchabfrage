f380b00e209de1e9ee7c4b59974d395572190e31
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-01-03 14:26:33 +0100

    Buchabfrage: Load-Balancing funktioniert!

--- Buchabfrage/src/main/java/de/eldecker/spring/buchabfrage/frontend/ThymeleafController.java
+++ Buchabfrage/src/main/java/de/eldecker/spring/buchabfrage/frontend/ThymeleafController.java
@@ -1,19 +1,34 @@
 package de.eldecker.spring.buchabfrage.frontend;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import de.eldecker.spring.buchabfrage.logik.PreisAbfrageClient;
 
+
+/**
+ * Controller für Thymeleaf-Templates: jede mit einem Mapping annotierte Methode
+ * gibt einen String mit dem Namen der anzuzeigenden Template-Datei zurück.
+ */
 @Controller
 public class ThymeleafController {
 	
 	private static Logger LOG = LoggerFactory.getLogger( ThymeleafController.class );
-
+	 	
+	/**
+	 * Client um Buchpreis von REST-API mit Load Balancing abzufragen. 
+	 */
+	@Autowired
+	private PreisAbfrageClient _preisAbfrageClient;
+	
+	
 	/**
 	 * Seite mit Details zu einem Buch anzeigen.
 	 * <br><br>
@@ -41,7 +56,11 @@ public class ThymeleafController {
     	    	    	
     	model.addAttribute( "isbn", isbn13 );
     	
-    	model.addAttribute( "preis", 59.99 );
+    	final Optional<Double> preisOptional = _preisAbfrageClient.holePreis( isbn13 );
+    	preisOptional.ifPresentOrElse( preisEuro -> model.addAttribute( "preis", preisEuro ), 
+    			                       ()        -> model.addAttribute( "preis", "???"     )
+    			                     );
+    	
     	
     	return "ergebnis";
     }
---
+++ Buchabfrage/src/main/java/de/eldecker/spring/buchabfrage/logik/PreisAbfrageClient.java
@@ -0,0 +1,79 @@
+package de.eldecker.spring.buchabfrage.logik;
+
+import de.eldecker.spring.buchabfrage.helferlein.Bohnenfabrik;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+
+/**
+ * Bean, um Preis für ein Buch von REST-API mit client-seitigem Load Balancing abzufragen.
+ */
+@Component
+public class PreisAbfrageClient {
+
+	private static Logger LOG = LoggerFactory.getLogger( PreisAbfrageClient.class );
+	
+	/** 
+	 * REST-Client-Objekt, konfiguriert für Load-Balancing mit Annotation 
+	 * {@code LoadBalanced} in Klasse {@link Bohnenfabrik#restTemplateMitLoadBalancing()).
+	 */
+	@Autowired
+	private RestTemplate _restTemplate;
+
+
+	/**
+	 * Bei REST-API mit Load Balancing Preis für Buch abfragen
+	 * 
+	 * @param isbn13 ISBN13 muss schon normiert sein (d.h. evtl. Bindestriche entfernt)
+	 * 
+	 * @return Optional enthält bei erfolgreicher Abfrage den Buchpreis in Euro
+	 */
+	public Optional<Double> holePreis( String isbn13 ) {
+
+        final String url = 
+        		UriComponentsBuilder.fromUriString( "http://isbnabfrage/api/v1/isbn2preis" ) // Pseudo-Domain "isbn-abfrage"
+                                    .queryParam( "isbn13", isbn13 )
+                                    .toUriString();        
+        try {
+        
+        	final ResponseEntity<Double> responseEntity = 
+        			_restTemplate.getForEntity( url, Double.class );
+        	
+            final HttpStatusCode statusCode = responseEntity.getStatusCode();
+            if ( statusCode.is2xxSuccessful() ) {
+
+            	final Double preisEuro = responseEntity.getBody();
+            	
+            	LOG.info( "Preis für Buch mit ISBN13={} von REST-API erhalten: {}", 
+            			  isbn13, preisEuro );
+            	
+            	return Optional.of( preisEuro );
+
+            } else {
+
+            	LOG.error( "HTTP-GET-Request für Abfrage Buchpreis lieferte Fehlercode {} zurück.", 
+            			   statusCode );
+            	return Optional.empty();
+            }
+        }
+        catch ( RestClientException ex ) {
+
+        	LOG.error( "HTTP-GET-Request für Abfrage Buchpreis für ISBN13={} hat Ausnahme geworfen.", 
+        			   isbn13, ex );
+        	return Optional.empty();
+        }
+	}
+
+	
+}
